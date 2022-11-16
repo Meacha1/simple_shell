50a4280baee24af2010db7c47ddcfc908d40aff4
Meacha1 <meachattd@gmail.com>
2022-11-15 22:21:49 -0800

    commit message

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdarg.h>
+
+char **tok_arg(char *input);
+int count_occurrence(char * str, char ch);
+int _setenv(char *input);
+int _chdir(const char *path);
+void _env(char **envp);
+char **tok(char *input);
+int ececute(char **parsed);
+
+#endif
